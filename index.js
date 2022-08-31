8e1198ccb0f3b1521f5f96fbaaf67dc792b28571
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-08-31 11:51:28 +0300

    Создал объект Солдат, добавил ему базовые свойства, здоровье, оружие, боеприпасы. После этого добавил методы, стрелять, перезарядить, ранить.

---
+++ index.js
@@ -0,0 +1,15 @@
+let solider = {
+    name: "Mans",
+    health: 10,
+    weapon: {name: "pistol", num_rounds: 30,},
+    supplies: 3,
+    fire: function(){
+
+    },
+    recharge: function(){
+
+    }, 
+    hurt: function(){
+
+    }
+}
