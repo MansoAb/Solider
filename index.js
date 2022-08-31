2f16fd098770bef6592b6b8722d2a53f4f903cf6
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-08-31 11:59:49 +0300

    Доработал методы стрелять, ранить, перезарядить.

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 let solider = {
     name: "Mans",
     health: 10,
-    weapon: {name: "pistol", num_rounds: 30,},
+    weapon: {name: "pistol", rounds: 30,},
     supplies: 3,
     fire: function(){
-
+        if(this.weapon.rounds > 0){
+        this.weapon.rounds -= 1
+        console.log("бах-бах")
+        }else{
+            return "обойма пуста. Перезаредитесь"
+    }
     },
     recharge: function(){
-
+        if(this.supplies > 0){
+            this.weapon.rounds = 30
+            this.supplies -= 1
+            console.log("Перезарядка...")
+        }else{
+            return "не осталось припасов"
+        }
     }, 
     hurt: function(){
+        if(this.health > 0){
+            this.health -= 1
+            if(this.health = 0){
+                console.log("Ты проиграл")
+            }
+        }
 
     }
 }
