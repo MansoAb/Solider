8a7124a4e93db43a5d81a13b0d7b691678cd130d
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-08-31 12:01:05 +0300

    Отформатировал код

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 let solider = {
-    name: "Mans",
-    health: 10,
-    weapon: {name: "pistol", rounds: 30,},
-    supplies: 3,
-    fire: function(){
-        if(this.weapon.rounds > 0){
-        this.weapon.rounds -= 1
-        console.log("бах-бах")
-        }else{
-            return "обойма пуста. Перезаредитесь"
+  name: "Mans",
+  health: 10,
+  weapon: { name: "pistol", rounds: 30 },
+  supplies: 3,
+  fire: function () {
+    if (this.weapon.rounds > 0) {
+      this.weapon.rounds -= 1;
+      console.log("бах-бах");
+    } else {
+      return "обойма пуста. Перезаредитесь";
     }
-    },
-    recharge: function(){
-        if(this.supplies > 0){
-            this.weapon.rounds = 30
-            this.supplies -= 1
-            console.log("Перезарядка...")
-        }else{
-            return "не осталось припасов"
-        }
-    }, 
-    hurt: function(){
-        if(this.health > 0){
-            this.health -= 1
-            if(this.health = 0){
-                console.log("Ты проиграл")
-            }
-        }
-
+  },
+  recharge: function () {
+    if (this.supplies > 0) {
+      this.weapon.rounds = 30;
+      this.supplies -= 1;
+      console.log("Перезарядка...");
+    } else {
+      return "не осталось припасов";
     }
-}
+  },
+  hurt: function () {
+    if (this.health > 0) {
+      this.health -= 1;
+      if ((this.health = 0)) {
+        console.log("Ты проиграл");
+      }
+    }
+  },
+};
